96f1a7b75360a5a4e802edbd972d408a6e1a9cc8
Lars B <lars.borntraeger@gmx.de>
2021-08-29 14:44:27 +0200

    add line breaks

--- poetry_client.py
+++ poetry_client.py
@@ -1,7 +1,12 @@
+#IMPORTANT: fpdf has to be installed (pip install fpdf)
+
 import tkinter as tk
 import random
 from tkinter import ttk
 import os
+from tkinter import font
+
+import fpdf
 random.seed()
 
 # set working directory to current directory
@@ -78,10 +83,22 @@ def button_click():
         entry_lines = entry_text.splitlines()
         pdf = FPDF("P", "mm", "A4")
         pdf.add_page()
-        pdf.set_font("Courier", "B", 24)
-        pdf.set_y(297/2-len(entry_lines)*10/2)
+        font_size = 24
+        pdf.set_font("Courier", "B", font_size)
+        lines_broken = list()
         for line in entry_lines:
+            mylen = len(line)
+            i = 0
+            while i < mylen:
+                lines_broken.append(line[i:i+35])
+                i += 35
+        pdf.set_y(297/2-len(lines_broken)*10/2)
+        for line in lines_broken:
             pdf.cell(190, 10 , line, 0, 1, "C")
+
+            
+
+
         pdf.output("poems/{:04d}.pdf".format(number))
         words_list = get_words()
         lbl_w0.config(text=words_list[0])
